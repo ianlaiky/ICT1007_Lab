c04166cede6a0ed4652299afcece25ae2b5f4724
ian <lai.kheng.yan@gmail.com>
2020-03-14 06:30:46 -0700

    fsd

--- Lab4/lab4.c
+++ Lab4/lab4.c
@@ -2,14 +2,27 @@
 #include "buffer.h"
 #include <stdlib.h>/* required for rand() */
 #include <pthread.h>
+#include <unistd.h>
 
+// todo delete later
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
 /* the buffer */
 buffer_item buffer[BUFFERSIZE];
 
 int insert_item(buffer_item item) {
+    int returntype = -1;
 /* insert item into buffer */
     printf("producer produced %d\n", item);
+    for (int i = 0; i < BUFFERSIZE; ++i) {
+        if (buffer[i] != 0) {
+            buffer[i] = item;
+            returntype = 0;
+            break;
+        }
+    }
 /* return 0 if successful, otherwise, return -1 indicating an error condition */
+    return returntype;
 }
 
 int remove_item(buffer_item *item) {
@@ -23,26 +36,30 @@ int remove_item(buffer_item *item) {
 void *producer(void *param) {
     buffer_item item;
     while (1) {
-        _sleep(rand); /* sleep for a random period of time */
+        printf("test run\n");
+        sleep(rand() % 50); /* sleep for a random period of time */
         item = rand(); /* generate a random number */
+
         if (insert_item(item) < 0)
             printf("…"); /* report error condition */
     }
 }
 
 ////consumer thread
-//void *consumer(void *param) {
-//    buffer_item item;
-//    while (1) {
-//        _sleep(rand); /* sleep for a random period of time */
-//        if (remove item(&item) < 0)
-//        printf("…"); /* report error condition */
-//    }
-//}
+void *consumer(void *param) {
+    buffer_item item;
+    while (1) {
+        sleep(rand()%50); /* sleep for a random period of time */
+        if (remove item(&item) < 0)
+        printf("…"); /* report error condition */
+    }
+}
 
 // pthreads
-void *thread_entry(void *param) { /* entry point of a new thread */
-    ...
+void *thread_entry_producer(void *param) { /* entry point of a new thread */
+    printf("thread created\n");
+
+    producer(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -68,20 +85,25 @@ int main(int argc, char *argv[]) {
     // buffer has been defined in header file
     int mutex = 1, full = 0, empty = BUFFERSIZE;
 
-//    printf("random No:%d\n", rand() % 50);
-//    printf("random No:%d\n", rand() % 50);
-//    printf("random No:%d\n", rand() % 50);
+    // set each of index in buffer to 0
+    for (int j = 0; j < BUFFERSIZE; ++j) {
+        buffer[j] = 0;
+    }
 
 /* 3. Create producer thread(s) */
     for (int i = 0; i < no_producer_threads; ++i) {
         pthread_t tid;
         pthread_attr_t attr;
         pthread_attr_init(&attr); /* get the default attribute */
-        pthread_create(&tid, &attr, thread_entry, NULL);
+        pthread_create(&tid, &attr, thread_entry_producer, NULL);
     }
 
 /* 4. Create consumer thread(s) */
 /* 5. Sleep */
+    sleep(time_to_sleep);
 /* 6. Release resources, e.g. destroy semaphores */
 /* 7. Exit */
 }
+
+//todo delete later
+#pragma clang diagnostic pop
