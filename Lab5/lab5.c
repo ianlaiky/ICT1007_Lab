3428c3b4576cd1134fffeabaa9f2f6351462ff89
ian <lai.kheng.yan@gmail.com>
2020-03-24 16:58:34 +0800

    cleanup

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_SIZE 256
-int main(){
 
-	int pageFrame;
-	
-	char *userInput = malloc(MAX_SIZE);
-	printf("Number of page frame: \n");
+int main() {
 
-	scanf("%d", &pageFrame);
-	
-	printf("Enter reference string: \n",userInput);	
+    int pageFrame;
+    int framearray[MAX_SIZE];
+    char *userInput = malloc(MAX_SIZE);
+    char *pch;
+    int indexposition = 0;
+    int pagefaultCounter = 0;
+    printf("Number of page frame: \n");
+
+    scanf("%d", &pageFrame);
+
+    printf("Enter reference string:(Include a space between pages E.g. 1 2 1 0) \n", userInput);
 
 
+    // to clear the input from scanf
+    fgets(userInput, MAX_SIZE, stdin);
+    // getting the user input
+    fgets(userInput, MAX_SIZE, stdin);
 
-  fgets(userInput, MAX_SIZE, stdin);
+    // using strtok to delimit by space
+    pch = strtok(userInput, " ");
+    while (pch != NULL) {
+        if (*pch != 10) {
+            int inter = atoi(pch);
 
- fgets(userInput, MAX_SIZE, stdin);
 
-printf("ref str: %s",userInput);
+            int pagefaultchck = 0;
+            for (int i = 0; i < pageFrame; ++i) {
+                if (framearray[i] == inter) {
+                    pagefaultchck++;
+                }
+            }
 
+            if (pagefaultchck == 0) {
+                pagefaultCounter++;
+                framearray[indexposition] = inter;
+                indexposition++;
+                if (indexposition + 1 == pageFrame) {
+                    indexposition = 0;
+                }
+            }
+
+        }
+        pch = strtok(NULL, " ");
+    }
+
+    printf("Page fault Occurred: %d\n",pagefaultCounter);
+    return 0;
 }
